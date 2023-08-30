1f1f7d28894c35250b68a128cb7d38710ad59ac1
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-08-29 23:39:56 -0600

    Argon2 Added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { Body, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(body: any) {
-    const user = await this.prisma.user.create({
-      data: {
-        name: body.name,
-        hash: body.password,
-        email: body.email,
-      },
-    });
-    console.log({ body });
-    // console.log(user);
-    return user;
+    const hash = await argon.hash(body.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: body.name,
+          hash,
+          email: body.email,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      //   return error.status(400).json({ message: error.message });
+    }
   }
 
   async signin() {
